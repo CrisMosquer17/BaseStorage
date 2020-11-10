15d55d119b2747bfb605bffd5eb607a4667e20e8
SpinxsS <inverso09@gmail.com>
2020-11-10 15:38:03 -0500

    Conexion a la base de datos de inpetel con mysql

---
+++ src/main/java/com/InpetelCloud/DBSTORAGES/DbstoragesApplication.java
@@ -0,0 +1,13 @@
+package com.InpetelCloud.DBSTORAGES;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbstoragesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbstoragesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/InpetelCloud/DBSTORAGES/DbstoragesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.InpetelCloud.DBSTORAGES;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbstoragesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
