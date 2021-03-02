9465cf2cb4f8744de188eb20f3ba91d9266ee85b
SpinxsS <inverso09@gmail.com>
2021-03-02 17:07:44 -0500

    arreglo error en la consulta

--- src/main/java/com/InpetelCloud/Dao/ReportesDao.java
+++ src/main/java/com/InpetelCloud/Dao/ReportesDao.java
@@ -26,7 +26,7 @@ public class ReportesDao implements ReportesInterface {
 				+ "M.Serial,\r\n"
 				+ "(SUBSTRING(MS.Fecha, 1, 20)) as Fecha,\r\n"
 				+ "GROUP_CONCAT(IF( MS.InfoMedidas_ID = '8', MS.Num_val ,NULL)) AS 'BC',\r\n"
-				+ "M.Magnitud,\r\\n"
+				+ "M.Magnitud,\r\n"
 				+ "GROUP_CONCAT(IF( MS.InfoMedidas_ID='2' , MS.Num_val ,NULL )) AS 'AE',\r\n"
 				+ "GROUP_CONCAT(IF( MS.InfoMedidas_ID = '3' , MS.Num_val  ,NULL)) AS 'AE',\r\n"
 				+ "GROUP_CONCAT(IF(MS.InfoMedidas_ID = '4' , MS.Num_val , NULL)) AS 'R1',\r\n"
